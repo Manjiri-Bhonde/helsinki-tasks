ef9e8e500e0097cbfb65b0ae0b4ade04e696322e
Manjiri Bhonde <manjiri.bhonde@gmail.com>
2025-11-12 13:50:27 +0530

    Tasks 6.3 - 6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,31 @@
+import { useDispatch } from "react-redux";
+import { createAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const anecdt = event.target.anecdt.value;
+    event.target.anecdt.value = "";
+    dispatch(createAnecdote(anecdt));
+  };
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form
+        onSubmit={(event) => {
+          addAnecdote(event);
+        }}
+      >
+        <div>
+          <input name="anecdt" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default AnecdoteForm;
